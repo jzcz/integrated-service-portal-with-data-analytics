4c90886b1564ba4686edd1851988235059ce34ff
Jazelle Cruz <cruz.jazelle.lapira@gmail.com>
2025-03-27 23:45:05 +0800

    add navbar component for student portal

---
+++ views/components/service-portal/navbar.php
@@ -0,0 +1,66 @@
+<!--- TOP NAVBAR (START) --->
+<nav class="navbar fixed-top">
+    <div class="container-fluid">
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#sidebar"
+        >
+            <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+        </button>
+        <a class="navbar-brand d-flex align-items-center gap-2" href="#">
+        <img src="../../../static\qcu-logo.jpg" alt="Logo" width="45" height="45" class="d-inline-block align-text-top">
+        <span class="navbar-brand mb-0 h1 align-self-center">QCU Guidance and Counseling Unit</span>
+        </a>           
+    </div>
+</nav>
+<!--- TOP NAVBAR (END) --->
+
+<!--- SIDEBAR (START) --->
+<div class="offcanvas offcanvas-start show" tabindex="-1" id="sidebar" aria-labelledby="offcanvasLabel">
+    <div class="offcanvas-body">
+        <nav class="">
+            <ul class="navbar-nav flex-column">
+                <li class="fw-bold nav-link-title">General</li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2 d-flex align-items-center gap-2">
+                    <i class="bi bi-megaphone-fill"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Announcements</a>
+                </li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-book-half"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Publication Materials</a>
+                </li>
+                <li class="my-1"><hr /></li>
+                <li class="fw-bold nav-link-title">Services</li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-calendar-fill"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Appointment Form</a>
+                </li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-pencil-fill"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Assessments</a>
+                </li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-ui-radios-grid"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Surveys</a>
+                </li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-file-earmark-fill"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Document Request</a>
+                </li>
+                <li class="my-1"><hr /></li>
+                <li class="fw-bold nav-link-title">Account</li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-person-fill"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Profile</a>
+                </li>
+                <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <i class="bi bi-power"></i>
+                    <a class="nav-link active" aria-current="page" href="#">Logout</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<!--- SIDEBAR (END) --->
