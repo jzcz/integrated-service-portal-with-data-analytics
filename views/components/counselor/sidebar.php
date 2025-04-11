3400217d997ceb105fdb96b87d97a773a372b278
Jazelle Cruz <cruz.jazelle.lapira@gmail.com>
2025-04-11 21:37:57 +0800

    add sidebar component

---
+++ views/components/counselor/sidebar.php
@@ -0,0 +1,54 @@
+
+<!--- SIDE NAVBAR START --->
+<div class="pt-4" id="sidebar">
+    <div class="sidebar-body d-flex flex-column gap-2">
+        <div class="sidebar-header text-center d-flex flex-column align-items-center gap-1">
+            <img src="../../static/qcu-logo-login.png" class="" alt="" width="75px" height="75  px">
+            <p class="fw-bold text-wrap text-center">QCU Guidance and Counseling Unit</p>
+        </div>
+        <div>
+            <nav>
+                <ul class="navbar-nav flex-column">
+                    <li class="fw-bold nav-link-title">General</li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2 d-flex align-items-center gap-2">
+                        <i class="bi bi-megaphone-fill"></i>
+                        <a class="nav-link active" aria-current="page" href="">Announcements</a>
+                    </li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-book-half"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Publication Materials</a>
+                    </li>
+                    <li class="my-1"><hr /></li>
+                    <li class="fw-bold nav-link-title">Services</li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-calendar-fill"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Appointment Form</a>
+                    </li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-pencil-fill"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Assessments</a>
+                    </li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-ui-radios-grid"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Surveys</a>
+                    </li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-file-earmark-fill"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Document Request</a>
+                    </li>
+                    <li class="my-1"><hr /></li>
+                    <li class="fw-bold nav-link-title">Account</li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-person-fill"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Profile</a>
+                    </li>
+                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                        <i class="bi bi-power"></i>
+                        <a class="nav-link active" aria-current="page" href="#">Logout</a>
+                    </li>
+                </ul> 
+            </nav>
+        </div>
+    </div>
+</div>    
+<!--- SIDE NAVBAR END --->
