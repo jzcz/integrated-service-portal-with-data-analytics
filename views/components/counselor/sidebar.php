ced8570dff0f0babf753412ee5f18afc046bcd3e
Jazelle Cruz <cruz.jazelle.lapira@gmail.com>
2025-04-11 22:47:42 +0800

    update navbar, add logout modal

--- views/components/counselor/sidebar.php
+++ views/components/counselor/sidebar.php
@@ -22,7 +22,7 @@
                     <li class="fw-bold nav-link-title">Services</li>
                     <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
                         <i class="bi bi-calendar-fill"></i>
-                        <a class="nav-link active" aria-current="page" href="#">Appointment Form</a>
+                        <a class="nav-link active" aria-current="page" href="#">Appointments</a>
                     </li>
                     <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
                         <i class="bi bi-pencil-fill"></i>
@@ -34,7 +34,7 @@
                     </li>
                     <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
                         <i class="bi bi-file-earmark-fill"></i>
-                        <a class="nav-link active" aria-current="page" href="#">Document Request</a>
+                        <a class="nav-link active" aria-current="page" href="#">Good Moral Certificate Requests</a>
                     </li>
                     <li class="my-1"><hr /></li>
                     <li class="fw-bold nav-link-title">Account</li>
@@ -42,9 +42,11 @@
                         <i class="bi bi-person-fill"></i>
                         <a class="nav-link active" aria-current="page" href="#">Profile</a>
                     </li>
-                    <li class="nav-item sidebar-nav-item d-flex align-items-center gap-2">
+                    <li class="py-2 nav-item sidebar-nav-item d-flex align-items-center gap-2">
                         <i class="bi bi-power"></i>
-                        <a class="nav-link active" aria-current="page" href="#">Logout</a>
+                        <button type="button" class="logout-btn w-100 text-start" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Logout
+                        </button>
                     </li>
                 </ul> 
             </nav>
@@ -52,3 +54,24 @@
     </div>
 </div>    
 <!--- SIDE NAVBAR END --->
+
+<!--- LOGIN MODAL START --->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header border-0">
+        <h1 class="modal-title fs-5 fw-bold" id="exampleModalLabel">
+            Log Out?
+        </h1>
+      </div>
+      <div class="modal-body border-0">
+        Are you sure you want to log out?
+      </div>
+      <div class="modal-footer border-0">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+        <a href=""><button type="button" class="btn btn-primary">Log Out</button></a>
+      </div>
+    </div>
+  </div>
+</div>
+<!--- LOGIN MODAL END --->
